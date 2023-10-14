1b705411e8293ae86036762f5746da81bfaca064
tanball <134679348+tanball@users.noreply.github.com>
2023-10-14 23:10:28 +0900

    up

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -33,28 +33,28 @@ public class PlayerMove : MonoBehaviour
     {
 
 
-        // # ì´ë™
+        // # ÀÌµ¿
         float h = 1.0f;
         float v = Input.GetAxis("Vertical");
+   
 
-
-        // ì´ë™ ê±°ë¦¬ ë³´ì • - ì„±ëŠ¥ì— ë”°ë¼ í•œ í”„ë ˆì„ì— ë‚˜ì˜¤ëŠ” ê²°ê³¼ ê°’ ë³´ì •
+        // ÀÌµ¿ °Å¸® º¸Á¤ - ¼º´É¿¡ µû¶ó ÇÑ ÇÁ·¹ÀÓ¿¡ ³ª¿À´Â °á°ú °ª º¸Á¤
         h = h * speed * Time.deltaTime;
         v = v * speed * Time.deltaTime;
 
 
-        // ì‹¤ì œ ì´ë™ 
+        // ½ÇÁ¦ ÀÌµ¿ 
         transform.Translate(Vector3.right * h);
 
 
 
-        // ì í”„
+        // Á¡ÇÁ
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (isjumping == false) // ë°”ë‹¥ì— ë‹¿ì•„ìˆì„ Â‹Âšë§Œ ì í”„ ê°€ëŠ¥í•˜ë„ë¡
+            if (isjumping == false) // ¹Ù´Ú¿¡ ´ê¾ÆÀÖÀ» ‹š¸¸ Á¡ÇÁ °¡´ÉÇÏµµ·Ï
             {
 
-                rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // ìœ„ìª½ìœ¼ë¡œ í˜ì„ ì¤Œ
+                rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // À§ÂÊÀ¸·Î ÈûÀ» ÁÜ
 
                 isjumping = true;
 
@@ -66,14 +66,14 @@ public class PlayerMove : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision) // ì¶©ëŒ 
+    private void OnCollisionEnter2D(Collision2D collision) // Ãæµ¹ 
     {
-        if (collision.gameObject.CompareTag("ground")) // ìºë¦­í„°ê°€ ë•…ì— ë‹¿ì•˜ì„ ë•Œ ì í”„ ê°€ëŠ¥
+        if (collision.gameObject.CompareTag("ground")) // Ä³¸¯ÅÍ°¡ ¶¥¿¡ ´ê¾ÒÀ» ¶§ Á¡ÇÁ °¡´É
         {
             isjumping = false;
         }
 
-        if (collision.gameObject.CompareTag("start")) // ì‹œì‘ ë•…ì„ ë°Ÿìœ¼ë©´ ë°°ê²½ìŒì•… í”Œë ˆì´!
+        if (collision.gameObject.CompareTag("start")) // ½ÃÀÛ ¶¥À» ¹âÀ¸¸é ¹è°æÀ½¾Ç ÇÃ·¹ÀÌ!
         {
             AudioManager.instance.PlayBGM("BGM_1");
         }
