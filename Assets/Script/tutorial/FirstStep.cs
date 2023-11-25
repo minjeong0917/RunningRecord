cb3f84b80efcb0f19c86099abfe5d503cf92cbab
tanball <134679348+tanball@users.noreply.github.com>
2023-11-25 15:32:57 +0900

    step1

--- Assets/Script/tutorial/FirstStep.cs
+++ Assets/Script/tutorial/FirstStep.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FirstStep : MonoBehaviour
@@ -10,6 +11,13 @@ public class FirstStep : MonoBehaviour
     float alpha = 1f;
     float fadeTime = 0.5f;
     //스텝1: 장애물 하나를 뛰어넘기
+    public GameObject health;
+    public TextMeshPro tmp;
+
+    private void Start()
+    {
+        
+    }
 
     void Update()
     {
@@ -28,13 +36,21 @@ public class FirstStep : MonoBehaviour
             Vector3 rightEndPosition = transform.position + transform.right * (transform.localScale.x / 2);
 
             Vector3 leftEdge = Camera.main.ViewportToWorldPoint(new Vector3(0.2f, 0.5f, 0));
-            
-            
+
+
             if (rightEndPosition.x < leftEdge.x)
             {
                 
                 objectPosition.x = objectPosition.x + delay;
                 transform.position = objectPosition;
+                if (health.GetComponent<PlayerHealth>().currentLives == 3)
+                {
+                    tmp.text = "잘하셨습니다!";
+                }
+                else
+                {
+                    health.GetComponent<PlayerHealth>().GetLife();
+                }
 
             }
         }
